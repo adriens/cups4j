f1d5bdafc16543251ed8d93315297c254a84bc33
salad74 <adrien.sales@gmail.com>
2013-10-28 02:31:49 +0000

    Ajout de la classe utilitaire qui imprime un fichier, en se basant uniquement sur le chemin du fichier. Classe utilisée pour imprimer depuis la tâche ant de test.

---
+++ src/main/java/org/cups4j/utils/PrintOnDemand.java
@@ -0,0 +1,144 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cups4j.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.InetAddress;
+import java.net.URL;
+import org.cups4j.CupsClient;
+import org.cups4j.CupsPrinter;
+import org.cups4j.PrintJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author salad74
+ */
+public class PrintOnDemand {
+    private static final Logger logger = LoggerFactory.getLogger(PrintOnDemand.class);
+    
+    private String host;
+    private int port;
+    private String printerName;
+    private String printerUrl;
+    
+    public PrintOnDemand(){
+        
+    }
+    public PrintOnDemand(String host, int port, String printerName){
+        setHost(host);
+        setPort(port);
+        setPrinterName(printerName);
+        setPrinterUrl("http://" + getHost() + ":" + getPort() + "/printers/" + getPrinterName());
+    }
+    
+    public void print(String filePath)throws Exception{
+        CupsClient cc = new CupsClient(getHost(),getPort());
+        CupsPrinter cp = cc.getPrinter(new URL(getPrinterUrl()));
+        //PrintJob pj = new PrintJob.Builder(commonVFSFileUrl).jobName("cups4j CommonsVFS direct printing test.").userName("cups4j").build();
+        File fileToPrint = new File(filePath);
+        FileInputStream fis = new FileInputStream(fileToPrint);
+        InetAddress localMachine = java.net.InetAddress.getLocalHost();
+        //System.out.println("Hostname of local machine: " + localMachine.getHostName());
+        PrintJob pj = new PrintJob.Builder(fis).jobName("cups4j CommonsVFS direct printing test.").userName(System.getProperty("user.name") + "@" + localMachine.getHostName()).build();
+
+        cp.print(pj);
+    }
+    
+    public static void main (String[] args){
+            
+        if(args == null){
+            logger.error("No args provided.");
+            System.exit(1);
+        }
+        else{
+            if(args.length < 4){
+                logger.error("Not enough args provided.");
+                System.exit(1);
+            }
+            else{
+                System.out.println("host : <" + args[0] + ">");
+                String host = args[0];
+                System.out.println("port : <" + args[1] + ">");
+                int port = Integer.parseInt(args[1]);
+                System.out.println("printer name : <" + args[2] + ">");
+                String printerName = args[2];
+                System.out.println("document commonVFS url : <" + args[3] + ">");
+                String vfsUrl = args[3];
+                
+                PrintOnDemand p = new PrintOnDemand(host, port, printerName);
+                try{
+                    p.print(vfsUrl);
+                    System.out.println("Print opration success.");
+                    System.exit(0);
+                }
+                catch(Exception ex){
+                    System.err.println("Unable to print : " + ex.getMessage());
+                    System.exit(1);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * @return the host
+     */
+    public String getHost() {
+        return host;
+    }
+
+    /**
+     * @param host the host to set
+     */
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    /**
+     * @return the port
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * @param port the port to set
+     */
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    /**
+     * @return the printerUrl
+     */
+    public String getPrinterUrl() {
+        return getPrinterName();
+    }
+
+    /**
+     * @param printerUrl the printerUrl to set
+     */
+    public void setPrinterUrl(String printerUrl) {
+        this.setPrinterName(printerUrl);
+    }
+
+    /**
+     * @return the printerName
+     */
+    public String getPrinterName() {
+        return printerName;
+    }
+
+    /**
+     * @param printerName the printerName to set
+     */
+    public void setPrinterName(String printerName) {
+        this.printerName = printerName;
+        
+    }
+}
